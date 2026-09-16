673ee32db378a306ac66050f2c85e5839ee30dee
Memona <maimonasiddiqi@gmail.com>
2026-09-16 14:09:59 +0500

    Secured API key using dotenv and added vault to gitignore

--- api.py
+++ api.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI,HTTPException,Depends
 from pydantic import BaseModel
 from scanner import PromptScanner
 from fastapi.security import APIKeyHeader
+from dotenv import load_dotenv
+import os 
 app=FastAPI()
 my_scanner=PromptScanner()
 key_header=APIKeyHeader(name="x-api-key")
-API_KEY = "Maimoona-is-learning-123"
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
 def verify_key( api_key: str = Depends(key_header)):
     if not api_key==API_KEY:
         raise HTTPException(status_code=403,detail="Invalid API Key")
